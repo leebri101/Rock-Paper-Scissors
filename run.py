0ee80f25522942131ffbbe667da89491e4edec5c
leebri101 <>
2023-05-16 12:48:16 +0000

    Added ASCII art for conditions

--- run.py
+++ run.py
@@ -20,7 +20,7 @@ def decide_winner(player_move, computer_move):
         return "It's a Tie!"
     elif (
         (player_move == "rock" and computer_move == "scissors") or
-        (player_move == "paper" and computer_move == "rock") or 
+        (player_move == "paper" and computer_move == "rock") or
         (player_move == "scissors" and computer_move == "paper")
     ):
         return "Player Wins!"
@@ -40,9 +40,9 @@ def display_title():
 
 def rules_page():
     print("\n" + colors.GREEN + "RULES OF THE GAME:")
-    print(colors.BLUE + "PLAYER MUST SELECT BETWEEN ROCK PAPER OR SCISSORS")
-    print(colors.BLUE + "THE COMPUTER THEN CHOOSES AT RANDOM")
-    print(colors.RED + "THE WINNER IS DECIDED BY 3 ROUNDS")
+    print(colors.BLUE + colors.BOLD + "PLAYER MUST SELECT BETWEEN ROCK PAPER OR SCISSORS")
+    print(colors.BLUE + colors.BOLD + "THE COMPUTER THEN CHOOSES AT RANDOM")
+    print(colors.RED + colors.BOLD + "THE WINNER IS DECIDED BY 3 ROUNDS")
     print()
 
 
@@ -57,23 +57,58 @@ def play_game():
 
     while True:
         for round in range(1, total_rounds + 1):
-
-
-"""
-
-    while True:
-        for round in range(1, total_rounds + 1):
             print(f"Round {round}:")
             player_move = get_player_move()
 
-            # Randomly generate the computer's move
+            # Random generation of computer's move
             computer_move = random.choice(valid_moves)
 
-            print("Player chooses:", player_move)
-            print("Computer chooses:", computer_move)
+            print(f"{colors.BLUE}Player Chose: {player_move}{colors.END}")
+            print(f"{colors.RED}Computer Chose: {computer_move}{colors.END}")
 
-            result = determine_winner(player_move, computer_move)
+            result = decide_winner(player_move, computer_move)
             print(result)
+
+            if result == "Player Wins!":
+                player_score += 1
+            elif result == "Computer Wins...":
+                computer_score += 1
+        
+        print(f"{colors.CYAN}{colors.BOLD}FINAL SCORES:")
+        print(f"{colors.BLUE}Player Score: {player_score}{colors.END}")
+        print(f"{colors.RED}Computer Score: {computer_score}{colors.END}")
+
+        if player_score > computer_score:
+            print(r"""
+            ██╗   ██╗ ██████╗ ██╗   ██╗    ██╗    ██╗██╗███╗   ██╗██╗██╗
+            ╚██╗ ██╔╝██╔═══██╗██║   ██║    ██║    ██║██║████╗  ██║██║██║
+             ╚████╔╝ ██║   ██║██║   ██║    ██║ █╗ ██║██║██╔██╗ ██║██║██║
+              ╚██╔╝  ██║   ██║██║   ██║    ██║███╗██║██║██║╚██╗██║╚═╝╚═╝
+               ██║   ╚██████╔╝╚██████╔╝    ╚███╔███╔╝██║██║ ╚████║██╗██╗
+               ╚═╝    ╚═════╝  ╚═════╝      ╚══╝╚══╝ ╚═╝╚═╝  ╚═══╝╚═╝╚═╝
+            """)
+        elif player_score < computer_score:
+            print(r"""
+            ██╗   ██╗ ██████╗ ██╗   ██╗   ██╗      ██████╗ ███████╗███████╗
+            ╚██╗ ██╔╝██╔═══██╗██║   ██║   ██║     ██╔═══██╗██╔════╝██╔════╝
+             ╚████╔╝ ██║   ██║██║   ██║   ██║     ██║   ██║███████╗█████╗
+              ╚██╔╝  ██║   ██║██║   ██║   ██║     ██║   ██║╚════██║██╔══╝        
+               ██║   ╚██████╔╝╚██████╔╝   ███████╗╚██████╔╝███████║███████╗
+               ╚═╝    ╚═════╝  ╚═════╝    ╚══════╝ ╚═════╝ ╚══════╝╚══════╝
+            """)
+        else:
+            print(r"""
+            ████████╗██╗███████╗
+            ╚══██╔══╝██║██╔════╝
+               ██║   ██║█████╗
+               ██║   ██║██╔══╝
+               ██║   ██║███████╗██╗██╗██╗██╗
+               ╚═╝   ╚═╝╚══════╝╚═╝╚═╝╚═╝╚═╝
+            """)
+
+
+
+"""
 
             if result == "Player wins!":
                 player_score += 1
