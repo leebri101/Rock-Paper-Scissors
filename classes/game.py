a69a91c786f85766d4ea6245837b7f9bc9f3be20
leebri101 <>
2023-05-16 11:17:37 +0000

    Added new folder to store classes

---
+++ classes/game.py
@@ -0,0 +1,16 @@
+import random
+import pprint
+
+
+# Colors and styling
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    WARNING = '\033[93m'
+    GREEN = '\033[92m'
+    RED = '\033[91m'
+    WHITE = '\033[37m'
+    CYAN = '\033[36m'
+    MAGENTA = '\033[35m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
